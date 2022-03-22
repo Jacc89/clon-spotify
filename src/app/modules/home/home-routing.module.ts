5514df2795f600bd7b41690edaa9f873e47da93a
jacc89 <laflacalex9@gmail.com>
2022-03-22 15:32:52 -0500

    siderbar

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// components de api
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePagesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
