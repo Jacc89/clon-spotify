7b992fe22be21985ecf9d3e29297c1bdf4913636
jacc89 <laflacalex9@gmail.com>
2022-03-22 21:59:18 -0500

    tracks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePagesComponent } from '@modules/home/pages/home-pages/home-pages.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: ()=> import(`./modules/home/home.module`).then(m => m.HomeModule), }
+  { path: 'auth', loadChildren: ()=> import(`./modules/auth/auth.module`).then(m => m.AuthModule) },
+  { path: '', component: HomePagesComponent, loadChildren: ()=> import(`./modules/home/home.module`).then(m => m.HomeModule) },
+
 
 ];
 
